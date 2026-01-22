faf4e4e2516af20f29c172f7347ea13a14fe265c
cassiebuhler <>
2026-01-22 21:21:37 +0000

    cleaning up

--- cpad/download.sh
+++
@@ -1,7 +0,0 @@
-
-
-
-wget https://data.cnra.ca.gov/dataset/31b65732-941d-4af0-9d8c-279fac441fd6/resource/f906894e-bc00-4152-aba9-b33ac2934e4e/download/cced_2024b_release.zip
-unzip cced_2024b_release.zip
-wget https://data.cnra.ca.gov/dataset/0ae3cd9f-0612-4572-8862-9e9a1c41e659/resource/27323846-4000-42a2-85b3-93ae40edeff9/download/cpad_2024b_release.zip
-unzip cpad_2024b_release.zip
